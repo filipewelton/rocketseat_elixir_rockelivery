018821ff5c55f13fe2ce60670906327a102b69a1
filipewelton1 <filipewelton1@gmail.com>
2023-12-31 18:20:57 -0300

    initial commit

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -1,6 +1,10 @@
 defmodule Rockelivery.DataCase do
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+  alias Ecto.Changeset
+  alias Rockelivery.Repo
+
   using do
     quote do
       alias Rockelivery.Repo
@@ -17,26 +21,17 @@ defmodule Rockelivery.DataCase do
     :ok
   end
 
-  @doc """
-  Sets up the sandbox based on the test tags.
-  """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Rockelivery.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
-  @doc """
-  A helper that transforms changeset errors into a map of messages.
-
-      assert {:error, changeset} = Accounts.create_user(%{password: "short"})
-      assert "password is too short" in errors_on(changeset).password
-      assert %{password: ["password is too short"]} = errors_on(changeset)
-
-  """
   def errors_on(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
+    Changeset.traverse_errors(changeset, fn {message, opts} ->
       Regex.replace(~r"%{(\w+)}", message, fn _, key ->
-        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+        opts
+        |> Keyword.get(String.to_existing_atom(key), key)
+        |> to_string()
       end)
     end)
   end
